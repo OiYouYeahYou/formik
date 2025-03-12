5339ddcdb9d8193e2575caa463b7128ec4ffd3b5
Jason <code@jasonallan.dev>
2025-03-12 16:17:35 +0000

    Naive dark mode re-theme
    This screws with contrast in negative ways but shows where work can be done

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,8 +1,36 @@
 // tailwind.config.js
 
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: ['./src/**/*.ts', './src/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        gray: inverse(colors.gray),
+        blue: inverse(colors.blue),
+        white: '#000',
+        black: '#fff',
+      },
+    },
+  },
 
   plugins: [require('@tailwindcss/forms')],
 };
+
+function inverse(obj) {
+  const keys = Object.keys(obj)
+    .map(v => parseInt(v))
+    .sort((a, b) => {
+      if (a === b) return 0;
+      return a < b ? 1 : -1;
+    });
+  const n = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    n[keys[i]] = obj[keys[keys.length - i - 1]];
+  }
+
+  return n;
+}
